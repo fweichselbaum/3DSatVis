10fc79592ac4e37101c21e49f60b6812f948b6e0
fweichselbaum <fabian.weichselbaum@gmx.at>
2026-01-19 16:13:35 +0100

    add ray (wip)

--- main.py
+++ main.py
@@ -69,13 +69,13 @@ class SatelliteVisualizer(ShowBase):
 
         self.gamepad = None
         self.setup_gamepad()
-        self.setup_selection_ray()
 
         self.load_omm_model()
 
         self.setup_shader()
-
-        self.accept("<space>", self.process_selection)
+        self.setup_ray()
+        
+        self.accept("space", self.process_selection)
         self.accept("gamepad-face_a", self.process_selection)
         self.taskMgr.add(self.process_inputs, "input")
         self.taskMgr.add(self.render_satellites, "render")
@@ -157,16 +157,6 @@ class SatelliteVisualizer(ShowBase):
         self.earth.reparentTo(self.render)
 
 
-    def setup_selection_ray(self):
-        self.selection_ray = LineSegs()
-        self.selection_ray.setThickness(2.0)
-        self.selection_ray.setColor(1, 0, 0, 1) # Red color
-        self.selection_ray.moveTo(0, 0, 0)
-        self.selection_ray.drawTo(0, 100, 0) # Initial direction
-        self.selection_ray_node = self.render.attachNewNode(self.selection_ray.create())
-        self.selection_ray_node.hide()
-
-
     def setup_gamepad(self):
         self.accept("connect-device", self.connect_device)
         self.accept("disconnect-device", self.disconnect_device)
@@ -244,87 +234,33 @@ class SatelliteVisualizer(ShowBase):
         self.points_np.setTransparency(TransparencyAttrib.MNone) # TODO test 
 
 
-    def render_satellites(self, task):
-        if task.frame % 2 == 0:
-            return task.cont
-
-        time = datetime.now(timezone.utc)
-        jd, fr = jday_datetime(time)
-        orbits = SatrecArray(self.satellite_orbits).sgp4(np.array([jd]), np.array([fr]))
-        _, positions, velocities = orbits
-
-        vertex_writer = GeomVertexWriter(self.vertex_data, "vertex")
-        vertex_writer.setRow(0)
-
-        self.scaled_positions = (positions * SCALE).astype(np.float32)
-        num_rows = len(self.scaled_positions)
-        self.vertex_data.setNumRows(num_rows)
-        array_handle = self.vertex_data.modifyArray(0)
-        memory_view = memoryview(array_handle)
-        np_buffer = np.frombuffer(memory_view, dtype=np.float32)
-        np_buffer[:] = self.scaled_positions.flatten()
-
-        return task.cont
-
-
-    def process_inputs(self, task):
-        dt = self.clock.dt
-        orbit_speed = 90.0 * dt
-        zoom_speed = 100.0 * dt
+    def setup_ray(self):
+        self.camLens.setNearFar(1.0, 10000.0)
+        self.ray_h = 0.0
+        self.ray_p = 0.0
+        # Node attached to camera to track its movement, we rotate it locally for the ray
         
-        h_input, p_input = 0.0, 0.0
-        zoom_input = 0.0
-        
-        # Gamepad Input
-        if self.gamepad:
-            g_lx = self.gamepad.findAxis(InputDevice.Axis.left_x).value
-            g_ly = self.gamepad.findAxis(InputDevice.Axis.left_y).value
-            
-            # Triggers: 0 to 1 range usually
-            l_trig = self.gamepad.findAxis(InputDevice.Axis.left_trigger).value
-            r_trig = self.gamepad.findAxis(InputDevice.Axis.right_trigger).value
-            
-            if abs(g_lx) > 0.1: h_input += g_lx 
-            if abs(g_ly) > 0.1: p_input += g_ly
-            
-            if abs(l_trig) > 0.05: zoom_input += l_trig
-            if abs(r_trig) > 0.05: zoom_input -= r_trig
+        self.ray_pivot = self.camera.attachNewNode("ray_pivot")
 
+        # Create the line pointing forward (+Y)
+        segs = LineSegs()
+        segs.setColor(1, 0, 1, 1)           # purple
+        segs.setThickness(3.0)
+        segs.moveTo(0, 0, 0)
+        segs.drawTo(0, 5000, 0)             # long line
 
-        # Keyboard Input
-        mw = self.mouseWatcherNode
-        if mw.is_button_down(KeyboardButton.ascii_key('w')): p_input += 1.0
-        if mw.is_button_down(KeyboardButton.ascii_key('s')): p_input -= 1.0
-        if mw.is_button_down(KeyboardButton.ascii_key('d')): h_input += 1.0
-        if mw.is_button_down(KeyboardButton.ascii_key('a')): h_input -= 1.0
-        
-        if mw.is_button_down(KeyboardButton.ascii_key('q')): zoom_input += 1.0
-        if mw.is_button_down(KeyboardButton.ascii_key('e')): zoom_input -= 1.0
-        
-        
-        # Update Orbit State
-        self.orbit_h += h_input * orbit_speed
-        self.orbit_p += p_input * orbit_speed
-        self.orbit_distance += zoom_input * zoom_speed
-        
-        # Clamp
-        self.orbit_p = max(-89.0, min(89.0, self.orbit_p))
-        self.orbit_distance = max(10.0, min(500.0, self.orbit_distance))
-        
-        self.update_camera_pos()
-        
-        # Update Visual Ray
-        self.selection_ray_node.show()
-        self.selection_ray_node.setPos(self.camera.getPos(self.render))
-        self.selection_ray_node.setQuat(self.camera.getQuat(self.render))
-        self.selection_ray_node.setScale(self.orbit_distance * 0.9) 
+        ray_geom = NodePath(segs.create())
+        ray_geom.setLightOff()
+        ray_geom.setTransparency(TransparencyAttrib.MAlpha)
+        ray_geom.setTwoSided(True)
+        ray_geom.reparentTo(self.ray_pivot)
 
-        return task.cont
-    
 
     def process_selection(self):
         cam_pos = np.array(self.camera.getPos(self.render), dtype=np.float32)
-        quat = self.camera.getQuat(self.render)
+        
+        # Get ray direction from the visual ray node
+        quat = self.ray_np.getQuat(self.render)
         fwd = quat.getForward()
         ray_dir = np.array([fwd.x, fwd.y, fwd.z], dtype=np.float32)
 
@@ -357,5 +293,91 @@ class SatelliteVisualizer(ShowBase):
             self.selected_satellite = selected_id
             self.points_np.setShaderInput("selected_id", self.selected_satellite)
 
+
+    def render_satellites(self, task):
+        if task.frame % 2 == 0:
+            return task.cont
+
+        time = datetime.now(timezone.utc)
+        jd, fr = jday_datetime(time)
+        orbits = SatrecArray(self.satellite_orbits).sgp4(np.array([jd]), np.array([fr]))
+        _, positions, velocities = orbits
+
+        vertex_writer = GeomVertexWriter(self.vertex_data, "vertex")
+        vertex_writer.setRow(0)
+
+        self.scaled_positions = (positions * SCALE).astype(np.float32)
+        num_rows = len(self.scaled_positions)
+        self.vertex_data.setNumRows(num_rows)
+        array_handle = self.vertex_data.modifyArray(0)
+        memory_view = memoryview(array_handle)
+        np_buffer = np.frombuffer(memory_view, dtype=np.float32)
+        np_buffer[:] = self.scaled_positions.flatten()
+
+        return task.cont
+
+
+    def process_inputs(self, task):
+        dt = self.clock.dt
+        orbit_speed = 90.0 * dt
+        zoom_speed = 100.0 * dt
+        ray_speed = 90.0 * dt
+        
+        h_input, p_input = 0.0, 0.0
+        zoom_input = 0.0
+        ray_h_input, ray_p_input = 0.0, 0.0
+        
+        # Gamepad Input
+        if self.gamepad:
+            g_lx = self.gamepad.findAxis(InputDevice.Axis.left_x).value
+            g_ly = self.gamepad.findAxis(InputDevice.Axis.left_y).value
+            
+            # Triggers
+            l_trig = self.gamepad.findAxis(InputDevice.Axis.left_trigger).value
+            r_trig = self.gamepad.findAxis(InputDevice.Axis.right_trigger).value
+            
+            # Right Stick (Ray)
+            g_rx = self.gamepad.findAxis(InputDevice.Axis.right_x).value
+            g_ry = self.gamepad.findAxis(InputDevice.Axis.right_y).value
+
+            # Orbit (Left Stick)
+            if abs(g_lx) > 0.1: h_input += g_lx 
+            if abs(g_ly) > 0.1: p_input += g_ly
+            
+            # Zoom
+            if abs(l_trig) > 0.05: zoom_input += l_trig
+            if abs(r_trig) > 0.05: zoom_input -= r_trig
+            
+            # Ray
+            if abs(g_rx) > 0.1: ray_h_input -= g_rx
+            if abs(g_ry) > 0.1: ray_p_input += g_ry
+
+        # Keyboard Input
+        mw = self.mouseWatcherNode
+        if mw.is_button_down(KeyboardButton.ascii_key('w')): p_input += 1.0
+        if mw.is_button_down(KeyboardButton.ascii_key('s')): p_input -= 1.0
+        if mw.is_button_down(KeyboardButton.ascii_key('d')): h_input += 1.0
+        if mw.is_button_down(KeyboardButton.ascii_key('a')): h_input -= 1.0
+        
+        if mw.is_button_down(KeyboardButton.ascii_key('q')): zoom_input += 1.0
+        if mw.is_button_down(KeyboardButton.ascii_key('e')): zoom_input -= 1.0
+        
+        # Update Orbit State
+        self.orbit_h += h_input * orbit_speed
+        self.orbit_p += p_input * orbit_speed
+        self.orbit_distance += zoom_input * zoom_speed
+        
+        self.orbit_p = max(-89.0, min(89.0, self.orbit_p))
+        self.orbit_distance = max(10.0, min(500.0, self.orbit_distance))
+        
+        self.update_camera_pos()
+
+        # Update Ray
+        self.ray_h += ray_h_input * ray_speed
+        self.ray_p += ray_p_input * ray_speed
+        self.ray_pivot.setHpr(self.ray_h, self.ray_p, 0)
+
+        return task.cont
+    
 
 SatelliteVisualizer().run()
--- shader/satellites.vert
+++ shader/satellites.vert
@@ -6,13 +6,18 @@ in vec4 p3d_Vertex;
 in vec4 p3d_Color;
 
 uniform float point_size;
+uniform uint selected_id;
 
 out vec4 v_color;
 flat out uint vertex_id;
 
 void main() {
     gl_Position = p3d_ModelViewProjectionMatrix * p3d_Vertex;
-    gl_PointSize = max(2.0, point_size / gl_Position.w);
     v_color = p3d_Color;
     vertex_id = gl_VertexID;
+    if (selected_id == vertex_id) {
+        gl_PointSize = max(2.0, point_size / gl_Position.w) * 5.0;
+    } else {
+        gl_PointSize = max(2.0, point_size / gl_Position.w);
+    }
 }
